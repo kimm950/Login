63cb85086ef1c27e2b8cd0c3516a99fb20142305
Kimm <hyunsung.kim@zeals.co.jp>
2020-01-11 23:24:10 +0900

    initial setup

---
+++ src/Login.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const LoginWrapper = styled.div`
+
+`
+
+const Login = () => {
+  return (
+    <LoginWrapper />
+  )
+}
+export default Login
